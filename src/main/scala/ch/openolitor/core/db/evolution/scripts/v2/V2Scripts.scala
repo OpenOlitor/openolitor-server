ddafd33704351ddd392bdf7152d0289491c9f88f
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2018-02-21 15:14:50 +0100

    OO-sunu #9 adding the creditor identifier in the kontodaten model, db mapping and the db script to create the new column

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
@@ -157,5 +157,6 @@ object V2Scripts {
     OOBetrieb2_recalculate_aktive_inaktive_accounts.scripts ++
     OO_sunu_7_maintenance_mode_zero.scripts ++
     OO_sunu_4_person_category.scripts ++
-    OO_sunu_4_2_adding_price_to_abo.scripts
+    OO_sunu_4_2_adding_price_to_abo.scripts ++
+    OO_sunu_9_adding_creditor_identifier.scripts
 }
