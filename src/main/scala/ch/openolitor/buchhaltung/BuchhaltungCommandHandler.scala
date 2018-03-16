75e842e2e105ea1d890e28eee66c4746f8e02393
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2018-03-16 11:38:22 +0100

    OO-sunu #20 deleting comments and the pain 008 003 002 reference

--- src/main/scala/ch/openolitor/buchhaltung/BuchhaltungCommandHandler.scala
+++ src/main/scala/ch/openolitor/buchhaltung/BuchhaltungCommandHandler.scala
@@ -35,7 +35,6 @@ import ch.openolitor.core._
 import ch.openolitor.core.db.ConnectionPoolContextAware
 
 import ch.openolitor.buchhaltung.zahlungsimport.{ ZahlungsImportRecord, ZahlungsImportRecordResult }
-import ch.openolitor.buchhaltung.rechnungsexport.iso20022.Pain008_003_02_Export.exportPain008_003_02
 import ch.openolitor.core.db.AsyncConnectionPoolContextAware
 
 import ch.openolitor.buchhaltung.repositories.DefaultBuchhaltungReadRepositorySyncComponent
@@ -43,7 +42,6 @@ import ch.openolitor.buchhaltung.repositories.BuchhaltungReadRepositorySyncCompo
 
 object BuchhaltungCommandHandler {
   case class RechnungVerschickenCommand(originator: PersonId, id: RechnungId) extends UserCommand
-  case class Pain008_003_02CreationCommand(originator: PersonId, ids: Seq[RechnungId]) extends UserCommand
   case class RechnungenVerschickenCommand(originator: PersonId, ids: Seq[RechnungId]) extends UserCommand
   case class RechnungMahnungVerschickenCommand(originator: PersonId, id: RechnungId) extends UserCommand
   case class RechnungBezahlenCommand(originator: PersonId, id: RechnungId, entity: RechnungModifyBezahlt) extends UserCommand
--- src/main/scala/ch/openolitor/buchhaltung/BuchhaltungRoutes.scala
+++ src/main/scala/ch/openolitor/buchhaltung/BuchhaltungRoutes.scala
@@ -124,22 +124,11 @@ trait BuchhaltungRoutes extends HttpService with ActorReferences
               entity(as[RechnungenContainer]) { cont =>
                 onSuccess(buchhaltungReadRepository.getByIds(rechnungMapping, cont.ids)) { rechnungen =>
                   val xml = generatePain008_001_07(rechnungen)
-                  logger.debug(s"=================================================================== xml = $xml")
-                  //val stream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8))
-                  //storeToFileStore(RechnungExportDaten, Some("exportFile"), stream, "exportFile.xml") { (id, metadata) =>
-                  //  complete("File uploaded")
-                  //}
                   ???
                 }
               }
             }
           }
-        //val xml = generatePain008()
-        //storeToFileStore(RechnungExportDaten, None, new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)), "fileToDownload") { (fileId, meta) =>
-        //  createRechnungExportPain008("fileToDownload", Seq())
-        //}
-
-        //download(RechnungExportDaten, "fileToDownload")
       } ~
       path("rechnungen" / "aktionen" / "verschicken") {
         post {
