a91269282e9095235867828b19a1186394116bda
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2018-04-04 17:13:11 +0200

    OO_sunu #26 all rechnungen and kunden must have a payment type

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
@@ -236,6 +236,7 @@ object V2Scripts {
     OO_sunu_11_adding_payment_parameters_to_kunde.scripts ++
     OO_sunu_13_adding_payment_type_to_rechnung.scripts ++
     OO_sunu_14_new_zahlung_export_table.scripts ++
+    OO_sunu_26_adding_payment_types.scripts ++
     Seq(arbeitseinsatzDBInitializationScript) ++
     OO109_Arbeitseinsatz.scripts
 }
