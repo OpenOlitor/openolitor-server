33dd6c860057e8d645bfc01f18869ffc56bf6f69
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2018-04-13 15:39:03 +0200

    OO-sunu aktiv inaktiv accounts script added to the v2 file

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
@@ -163,5 +163,6 @@ object V2Scripts {
     OO_sunu_13_adding_payment_type_to_rechnung.scripts ++
     OO_sunu_14_new_zahlung_export_table.scripts ++
     OO_sunu_26_adding_payment_types.scripts ++
-    OO_sunu_27_adding_status_to_zahlungs_export.scripts
+    OO_sunu_27_adding_status_to_zahlungs_export.scripts ++
+    OOBetrieb2_recalculate_aktive_inaktive_accounts.scripts
 }
