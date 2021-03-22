86098ac8777ca05357f992f77847d1cfe9ef6c31
mike.toggweiler <mike.toggweiler@tegonal.com>
2021-03-22 08:11:54 +0100

    fixed after rebase

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/v2/V2Scripts.scala
@@ -252,5 +252,5 @@ object V2Scripts {
     OO411_adding_person_contact_permission.scripts ++
     OO86_enlarge_login_message_size.scripts ++
     OO86_create_kunden_specific_message.scripts ++
-    OO_add_otp_second_facor.scripts
+    OO_add_otp_second_factor.scripts
 }
