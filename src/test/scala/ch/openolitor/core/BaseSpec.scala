02b72717deb213159327fcaee007e9053935beb1
Thomas Rawyler <thomas.rawyler@tegonal.com>
2022-10-28 13:33:40 +0200

    OO#7: improved test infrastructure

--- src/test/scala/ch/openolitor/core/BaseSpec.scala
+++ src/test/scala/ch/openolitor/core/BaseSpec.scala
@@ -2,8 +2,10 @@ package ch.openolitor.core
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.testkit.{ RouteTestTimeout, Specs2RouteTest }
+import akka.testkit.TestProbe
 import ch.openolitor.core.db.WithInMemoryDatabase
 import ch.openolitor.core.filestore.MockFileStoreComponent
+import ch.openolitor.core.models.DBEvent
 import com.typesafe.scalalogging.LazyLogging
 import org.specs2.matcher.Matchers
 import org.specs2.mutable.Specification
@@ -19,14 +21,41 @@ trait BaseRoutesSpec extends BaseSpec with Specs2RouteTest with SprayJsonSupport
   import akka.testkit._
 
   implicit val timeout = RouteTestTimeout(defaultTimeout.dilated)
+
+  implicit val duration = 10 seconds
 }
 
+/**
+ * Extending this base spec will initialize a docker container using in-memory storage, see [[ch.openolitor.core.db.WithInMemoryDatabase]]
+ * and spinning up all the actors, see [[ch.openolitor.core.MockInMemoryActorReferences]].
+ *
+ * The database container is initialized once per class loader.
+ * Each spec class will have its own database with a random identifier within this container.
+ *
+ * The database part is designed to support pumping the snapshot for each test case in [[org.specs2.specification.Before#before()]].
+ * This snapshot is created once per spec.
+ * As you can see the default behaviour here is to pump it once in [[org.specs2.specification.BeforeAll#beforeAll()]].
+ * This is due to the fact that the actors are also created once in [[org.specs2.specification.BeforeAll#beforeAll()]].
+ *
+ * Feel free to extend this class and implement a reset of both the database and actors in [[org.specs2.specification.Before#before()]] to have a clean state for each test case.
+ */
 trait BaseRoutesWithDBSpec extends BaseRoutesSpec with WithInMemoryDatabase with StartingServices with MockInMemoryActorReferences {
+  sequential
+
   override def beforeAll() = {
     super.beforeAll()
 
     initializeConnectionPool()
 
     MockInMemoryActorReferences.initialize(config, sysConfig)
+  }
+
+  /**
+   * @return a [[akka.testkit.TestProbe]] listening on the eventStream for [[ch.openolitor.core.models.DBEvent]]s.
+   */
+  protected def dbEventProbe() = {
+    val probe = TestProbe()
+    MockInMemoryActorReferences.MockStartedServices(sysConfig).app.eventStream.subscribe(probe.ref, classOf[DBEvent[_]])
+    probe
   }
 }
--- src/test/scala/ch/openolitor/stammdaten/StammdatenRoutesProjektSpec.scala
+++ src/test/scala/ch/openolitor/stammdaten/StammdatenRoutesProjektSpec.scala
@@ -5,6 +5,7 @@ import ch.openolitor.core.{ BaseRoutesWithDBSpec, SpecSubjects }
 import ch.openolitor.core.security.Subject
 import ch.openolitor.stammdaten.models.{ Projekt, ProjektModify }
 import ch.openolitor.core.Macros._
+import ch.openolitor.core.models.EntityModified
 
 import scala.concurrent.Await
 
@@ -30,8 +31,16 @@ class StammdatenRoutesProjektSpec extends BaseRoutesWithDBSpec with SpecSubjects
       val projekt = Await.result(service.stammdatenReadRepository.getProjekt, defaultTimeout).get
       val update = copyTo[Projekt, ProjektModify](projekt).copy(bezeichnung = "Updated")
 
+      val probe = dbEventProbe()
+
       Post(s"/projekt/${projekt.id.id}", update) ~> service.stammdatenRoute ~> check {
         status === StatusCodes.Accepted
+
+        // wait for modification to happen
+        probe.expectMsgType[EntityModified[Projekt]]
+
+        val updated = Await.result(service.stammdatenReadRepository.getProjekt, defaultTimeout).get
+        updated.bezeichnung === "Updated"
       }
     }
   }
