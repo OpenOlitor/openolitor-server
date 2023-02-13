4554f7b3ddb3d89fc8656b49c6f4c329b70b8979
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2023-02-13 15:44:21 +0100

    OO adding kunden tests

--- src/test/scala/ch/openolitor/stammdaten/StammdatenRoutesKundenSpec.scala
+++ src/test/scala/ch/openolitor/stammdaten/StammdatenRoutesKundenSpec.scala
@@ -1,9 +1,9 @@
 package ch.openolitor.stammdaten
 
-import akka.http.scaladsl.server.Directives.onSuccess
 import ch.openolitor.core.Macros.copyTo
+import ch.openolitor.core.models.{ EntityCreated, EntityModified }
 import ch.openolitor.core.security.Subject
-import ch.openolitor.core.{BaseRoutesWithDBSpec, SpecSubjects}
+import ch.openolitor.core.{ BaseRoutesWithDBSpec, SpecSubjects }
 import ch.openolitor.stammdaten.models._
 
 import scala.concurrent.Await
@@ -17,35 +17,46 @@ class StammdatenRoutesKundenSpec extends BaseRoutesWithDBSpec with SpecSubjects 
 
   "StammdatenRoutes for Kunden and Personen" should {
     "create Kunden" in {
-      val create = KundeModify( true, Some("bezeichnung"), "strasse", Some("02"), None, "383832", "Bern", Some("Bern"), false, None, None, None, None, None, None, Some(""), Some(982121), Some(982121), Set.empty, Seq.empty   , Seq.empty   , None , None )
+      val person = PersonModify(None, None, "Name", "Vorname", None, None, None, None, Set.empty, None, None, false)
+      val create = KundeModify(true, Some("bezeichnung"), "strasse", Some("02"), None, "383832", "Bern", Some("Bern"), false, None, None, None, None, None, None, Some(""), None, None, Set.empty, Seq.empty, Seq(person), None, None)
 
       Post("/kunden", create) ~> stammdatenRouteService.stammdatenRoute ~> check {
-        expectDBEvents(1) { (creations, _, _, _) =>
-          oneEventMatches[Kunde](creations)(_.bezeichnung === "bezeichnung")
-        }
+        dbEventProbe.expectMsgType[EntityCreated[Kunde]]
+        dbEventProbe.expectMsgType[EntityCreated[KontoDaten]]
+        dbEventProbe.expectMsgType[EntityCreated[Person]]
+        dbEventProbe.expectNoMessage()
+
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getKunden(asyncConnectionPoolContext), defaultTimeout)
+        result.size === 2
+        result.head.bezeichnung === "bezeichnung"
       }
     }
 
     "get kunde" in {
       Get("/kunden") ~> stammdatenRouteService.stammdatenRoute ~> check {
-        val result = responseAs[List[Kunde]]
+        val result = responseAs[List[KundeUebersicht]]
 
-        result.size === 1
-        result.head.strasse === "strasse"
+        result.size === 2
+        result.find(k => k.bezeichnung == "bezeichnung").head.strasse === "strasse"
       }
     }
 
     "modify kunde" in {
-      val kunde = Await.result(stammdatenRouteService.stammdatenReadRepository.getKunden, defaultTimeout).head
+      val kunde = Await.result(stammdatenRouteService.stammdatenReadRepository.getKunden, defaultTimeout).find(k => k.bezeichnung == "bezeichnung").head
+      val person = Seq(PersonModify(None, None, "Name", "Vorname", None, None, None, None, Set.empty, None, None, false))
       val modify = copyTo[Kunde, Kunde](kunde).copy(strasse = "STRASSE")
-      val modify1 = copyTo[Kunde, KundeModify](modify, "pendenzen" -> Seq.empty, "ansprechpersonen" -> Seq.empty , "kontoDaten" -> None, "bezeichnung" -> None)
-//      val modify = copyTo[Kunde, KundeModify](modify,"bezeichnung" -> (Some(kunde.bezeichnung): Option[String]),"pendenzen" -> Seq.empty, "ansprechpersonen" -> Seq.empty , "kontoDaten" -> None)
+      val modify1 = copyTo[Kunde, KundeModify](modify, "pendenzen" -> Seq.empty, "ansprechpersonen" -> person, "kontoDaten" -> None, "bezeichnung" -> None)
 
       Post(s"/kunden/${kunde.id.id}", modify1) ~> stammdatenRouteService.stammdatenRoute ~> check {
-        expectDBEvents(1) { (_, modifications, _, _) =>
-          oneEventMatches[Produzent](modifications)(_.strasse === "STRASSE")
-        }
+        dbEventProbe.expectMsgType[EntityModified[Kunde]]
+        dbEventProbe.expectMsgType[EntityCreated[Person]]
+        dbEventProbe.expectNoMessage()
+
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getKunden(asyncConnectionPoolContext), defaultTimeout)
+        result.size === 2
+        result.head.strasse === "STRASSE"
       }
     }
+
   }
 }
