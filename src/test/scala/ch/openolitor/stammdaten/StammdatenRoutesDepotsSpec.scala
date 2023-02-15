ea0ceccd6925d8b988c5cd8f8f06a00d69cd9f0c
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2023-02-15 12:36:30 +0100

    OO Adding test on depots routes

---
+++ src/test/scala/ch/openolitor/stammdaten/StammdatenRoutesDepotsSpec.scala
@@ -0,0 +1,95 @@
+package ch.openolitor.stammdaten
+
+import ch.openolitor.core.Macros.copyTo
+import ch.openolitor.core.models.{ EntityCreated, EntityDeleted, EntityModified }
+import ch.openolitor.core.security.Subject
+import ch.openolitor.core.{ BaseRoutesWithDBSpec, SpecSubjects }
+import ch.openolitor.stammdaten.models._
+
+import scala.concurrent.Await
+
+class StammdatenRoutesDeposSpec extends BaseRoutesWithDBSpec with SpecSubjects with StammdatenRouteServiceInteractions with StammdatenJsonProtocol {
+  sequential
+
+  protected val stammdatenRouteService = new MockStammdatenRoutes(sysConfig, system)
+
+  implicit val subject: Subject = adminSubject
+
+  "StammdatenRoutes for Depots" should {
+    "create Depots" in {
+      val create = DepotModify("Depot", "DEP", None, None, None, None, None, None, None, None, None, None, "821", "Bern", false, None, None, None, None, None, None)
+      Post("/depots", create) ~> stammdatenRouteService.stammdatenRoute ~> check {
+        dbEventProbe.expectMsgType[EntityCreated[Depot]]
+        dbEventProbe.expectNoMessage()
+
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout)
+        result.size === 1
+        result.head.name === "Depot"
+      }
+    }
+
+    "get depot " in {
+      Get("/depots") ~> stammdatenRouteService.stammdatenRoute ~> check {
+        val result = responseAs[List[Depot]]
+
+        result.size === 1
+        result.head.name === "Depot"
+      }
+    }
+
+    "get all persons by depots" in {
+      Get("/depots/personen/alle") ~> stammdatenRouteService.stammdatenRoute ~> check {
+        val result = responseAs[List[PersonSummary]]
+
+        result.size === 1
+        result.head.name === "Administrator"
+        result.head.vorname === "System"
+      }
+    }
+
+    "get aktiv persons by depots" in {
+      Get("/depots/personen/aktiv") ~> stammdatenRouteService.stammdatenRoute ~> check {
+        val result = responseAs[List[PersonSummary]]
+        result.size === 0
+      }
+    }
+
+    "modify depot" in {
+      val depot = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout).head
+      val modify = copyTo[Depot, DepotModify](depot, "depotlieferungen" -> Seq.empty).copy(name = "DEPOT")
+
+      Post(s"/depots/${depot.id.id}", modify) ~> stammdatenRouteService.stammdatenRoute ~> check {
+        dbEventProbe.expectMsgType[EntityModified[Depot]]
+        dbEventProbe.expectNoMessage()
+
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout)
+        result.size === 1
+        result.head.name === "DEPOT"
+      }
+    }
+
+    "modify depot" in {
+      val depot = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout).head
+      val modify = copyTo[Depot, DepotModify](depot, "depotlieferungen" -> Seq.empty).copy(name = "DEPOT")
+
+      Put(s"/depots/${depot.id.id}", modify) ~> stammdatenRouteService.stammdatenRoute ~> check {
+        dbEventProbe.expectMsgType[EntityModified[Depot]]
+        dbEventProbe.expectNoMessage()
+
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout)
+        result.size === 1
+        result.head.name === "DEPOT"
+      }
+    }
+
+    "delete depot" in {
+      val depot = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout).head
+      Delete(s"/depots/${depot.id.id}") ~> stammdatenRouteService.stammdatenRoute ~> check {
+        dbEventProbe.expectMsgType[EntityDeleted[Tour]]
+        dbEventProbe.expectNoMessage()
+        val result = Await.result(stammdatenRouteService.stammdatenReadRepository.getDepots, defaultTimeout)
+        result.size === 0
+      }
+    }
+  }
+}
