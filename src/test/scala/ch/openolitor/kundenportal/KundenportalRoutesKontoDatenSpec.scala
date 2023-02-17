2b3df11c7e9d7f64a7fdc963d2c4402df86a9247
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2023-02-17 18:49:37 +0100

    OO - Adding test kontodaten kundeportal

--- src/test/scala/ch/openolitor/kundenportal/KundenportalRoutesKontoDatenSpec.scala
+++ src/test/scala/ch/openolitor/kundenportal/KundenportalRoutesKontoDatenSpec.scala
@@ -2,18 +2,15 @@ package ch.openolitor.kundenportal
 
 import ch.openolitor.buchhaltung.BuchhaltungJsonProtocol
 import ch.openolitor.core.db.WithWriteRepositories
-import ch.openolitor.core.filestore.FileType
 import ch.openolitor.core.security.Subject
-import ch.openolitor.core.{BaseRoutesWithDBSpec, SpecSubjects}
+import ch.openolitor.core.{ BaseRoutesWithDBSpec, SpecSubjects }
 import ch.openolitor.stammdaten.models._
-import ch.openolitor.stammdaten.{MockStammdatenRoutes, StammdatenJsonProtocol, StammdatenRouteServiceInteractions}
+import ch.openolitor.stammdaten.{ MockStammdatenRoutes, StammdatenJsonProtocol, StammdatenRouteServiceInteractions }
 
 import scala.concurrent.Await
 
-class KundenportalRoutesProjectSpec extends BaseRoutesWithDBSpec with SpecSubjects with StammdatenRouteServiceInteractions with StammdatenJsonProtocol with BuchhaltungJsonProtocol with WithWriteRepositories {
+class KundenportalRoutesKontoDatenSpec extends BaseRoutesWithDBSpec with SpecSubjects with StammdatenRouteServiceInteractions with StammdatenJsonProtocol with BuchhaltungJsonProtocol with WithWriteRepositories {
   sequential
-
-  import ch.openolitor.util.Fixtures._
 
   protected var oskiKunde: KundeDetail = null
   protected lazy val oskiSubject: Subject = Subject("oski", oskiKunde.ansprechpersonen.head.id, oskiKunde.id, Some(KundenZugang), None)
@@ -25,12 +22,12 @@ class KundenportalRoutesProjectSpec extends BaseRoutesWithDBSpec with SpecSubjec
     oskiKunde = setupProjekt()
   }
 
-  "KundenportalRoutes for Project" should {
-    "get project" in {
+  "KundenportalRoutes for KontoDaten" should {
+    "get KontoDaten" in {
       implicit val subject = oskiSubject
-      Get(s"/kundenportal/projekt") ~> kundenportalRouteService.kundenportalRoute ~> check {
-        val projekt = responseAs[Option[ProjektKundenportal]]
-        projekt.head.bezeichnung === "Demo Projekt"
+      Get(s"/kundenportal/kontodaten") ~> kundenportalRouteService.kundenportalRoute ~> check {
+        val kontodaten = responseAs[Option[KontoDaten]]
+        kontodaten.head.id === KontoDatenId(1)
       }
     }
   }
